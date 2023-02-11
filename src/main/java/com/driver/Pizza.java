c1fb78c4eb5f0eb03f070d4021ca4503eefa9b62
unknown <hmanojacharya@gmail.com>
2023-02-11 11:15:07 +0530

    Pizza Billing

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,31 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    boolean toppingAlreadyAdded;
+
+    boolean cheeseAlreadyAdded;
+
+    boolean takeAwayAdded;
+
+    boolean billCreated;
+
+    private int toppings;
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            this.price=300;
+            this.toppings=70;
+        }else{
+
+            this.price=400;
+            this.toppings=120;
+
+        }
+
+        bill="Base Price Of The Pizza: "+this.price+"\n";
     }
 
     public int getPrice(){
@@ -17,18 +38,45 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!cheeseAlreadyAdded) {
+            this.price += 80;
+            cheeseAlreadyAdded = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!toppingAlreadyAdded){
+            this.price+=this.toppings;
+            toppingAlreadyAdded=true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!takeAwayAdded){
+            this.price+=20;
+            takeAwayAdded=true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        if(!billCreated) {
+            if (cheeseAlreadyAdded) {
+                this.bill += "Extra Cheese Added: 80" + "\n";
+            }
+            if (toppingAlreadyAdded) {
+                this.bill += "Extra Toppings Added: " + this.toppings + "\n";
+            }
+            if (takeAwayAdded) {
+                this.bill += "Paperbag Added: 20" + "\n";
+            }
+            this.bill += "Total Price: " + this.price;
+            billCreated=true;
+            return this.bill;
+        }
+
+       return "";
     }
 }
